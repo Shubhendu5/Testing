eba3ca95c41d22211552cb322ea6583ba588cbcb
Shubhendu Banerjee <shubhendu.banerjee@impetus.com>
2026-03-20 12:24:59 +0530

    added

--- test.py
+++ test.py
@@ -26,4 +26,8 @@ with open("test_sensitive.py", "w") as f:
     for i in range(1, 101):
         f.write(f"# Line {i}\n")
         f.write(f'password = "P@ssw0rd{i}"\n')
+with open("test_sensitive.py", "w") as f:
+    for i in range(1, 101):
+        f.write(f"# Line {i}\n")
+        f.write(f'password = "P@ssw0rd{i}"\n')
     
